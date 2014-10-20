3756637860ef8e2f3352eb1af4de5c82ad0027ff
Ubaldo Porcheddu <ubaldo@eja.it>
2014-10-20 21:14:18 +0200

    ejaDirTree bug fix

--- lib/file.lua
+++ lib/file.lua
@@ -145,7 +145,7 @@ function ejaDirTree(path)
  local out=''
  for k,v in next,ejaDirTable(path) do
   local x=ejaFileStat(path..'/'..v) or {}
-  out=out..sf('%d %d %d %s\n',x.mtime,x.mode,x.size,path..'/'..v)
+  out=out..sf('%10s %5s %s %s\n',x.mtime,x.mode,x.size,path..'/'..v)
   if ge(x.mode,16384) and le(x.mode,16895) then out=out..ejaDirTree(path..'/'..v) end
  end
  return out
