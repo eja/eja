00c4642a374a90e1dad0ccc83de89b3ee3ce2f16
ubaldus <ubaldo@eja.it>
2021-03-19 12:56:52 +0100

    web auth optional

--- eja.lua
+++ eja.lua
@@ -7308,6 +7308,9 @@ end
 -- Copyright (C) 2007-2021 by Ubaldo Porcheddu <ubaldo@eja.it>
 
 
+eja.help.webAuth="web hash check {1}";	-- 0 no auth check, 1 auth check enabled, 2 allow remote ip to be passed by proxy as X-Real-IP
+
+
 function ejaWebAuthHashCheck(key, path, ip, hash)
  local auth=-1;
  local path=path or "";
@@ -7339,21 +7342,28 @@ end
 
 
 function ejaWebAuth(web)
- local auth,path=web.path:match('^/('..string.rep('%x',64)..')(/.*)$');
- if auth and path then
-  web.auth=-1;
-  web.path=path;
-  local powerMax=5;
-  local authData=ejaFileRead(eja.pathEtc..'eja.web');
-  local ip=web.remoteIp;
-  if authData then
-   for k,v in authData:gmatch('([%x]+) ?([0-9]*)\n?') do
-    if ejaNumber(v) > powerMax then powerMax=v; end
-    local value=ejaWebAuthHashCheck(k, path, ip, auth);
-    if value > 0 then
-     web.auth=value*v;
-     web.authKey=k;
-     break;
+ local level=tonumber(eja.opt.webAuth) or 1;
+ if level > 0 then
+  local auth,path=web.path:match('^/('..string.rep('%x',64)..')(/.*)$');
+  if auth and path then
+   web.auth=-1;
+   web.path=path;
+   local powerMax=5;
+   local authData=ejaFileRead(eja.pathEtc..'eja.web');
+   local ip=web.remoteIp;
+   local ipProxy=ejaString(web.headerIn["x-real-ip"]);
+   if authData then
+    for k,v in authData:gmatch('([%x]+) ?([0-9]*)\n?') do
+     if ejaNumber(v) > powerMax then powerMax=v; end
+     local value=ejaWebAuthHashCheck(k, path, ip, auth);
+     if value < 1 and level >= 2 and ipProxy ~= "" then     
+      value=ejaWebAuthHashCheck(k, path, ipProxy, auth);
+     end
+     if value > 0 then
+      web.auth=value*v;
+      web.authKey=k;
+      break;
+     end
     end
    end
   end
